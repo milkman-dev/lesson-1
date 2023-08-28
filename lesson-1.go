ea6861765525f704184963e530946fa16d56a22f
Logan Milkman <loganescobarc@gmail.com>
2023-08-27 23:17:30 -0600

    wtf go is weird to convert values

--- lesson-1.go
+++ lesson-1.go
@@ -5,11 +5,44 @@ import (
 )
 
 func IsClabeValid(clabe string) bool {
-	// pass
+
+	multiplication := []int{3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7}
+
+	var clabeSum int
+	var modMinus10 int
+	const storiPrefix string = "6461802244"
+	var indexMult int = 0
+	var multNum int
+
+	if len(clabe) != 18 {
+
+		return false
+
+	}
+
+	for a := range clabe[:17] {
+		clabeNum := int(a)
+		multNum = multiplication[indexMult]
+		fmt.Println("Multiplying", clabeNum, "*", multNum)
+		clabeMult := clabeNum * multNum
+		clabeSum += clabeMult
+		indexMult++
+		if indexMult > 17 {
+			break
+		}
+	}
+
+	mod10 := clabeSum % 10
+	// var clabePrefix bool = clabe[0:10] == storiPrefix
+
+	modMinus10 = (10 - mod10) % 10
+
+	return int(clabe[18]-'0') == modMinus10
+
 }
 
 func main() {
-	fmt.Print(IsClabeValid("646180224445015833")) // true
-	fmt.Print(IsClabeValid("646180224445015837")) // false
-	fmt.Print(IsClabeValid("646"))                // false
+	fmt.Println(IsClabeValid("646180224445015833")) // true
+	fmt.Println(IsClabeValid("646180224445015837")) // false
+	fmt.Println(IsClabeValid("646"))                // false
 }
